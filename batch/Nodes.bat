fc5149460951d9c05c6d69d0875dc87974b1899e
chwan-Lee <89828989+chwan-Lee@users.noreply.github.com>
2022-09-15 11:15:13 +0900

    HCTR+LCTR

--- batch/Nodes.bat
+++ batch/Nodes.bat
@@ -2,8 +2,8 @@ rem coverage calculation
 "C:\ATDI\HTZ warfare x64\HTZwx64.exe" "C:\HTZ PROJECT\INP.PRO" -ADMIN 1022 1 30
 
 rem : Search for nodes (Range Extenders) to interconnect isolated clusters - up to 5 nodes to search
-"C:\ATDI\HTZ warfare x64\HTZwx64.exe" "C:\HTZ PROJECT\INP.PRO" -ADMIN 1025 4 "C:\gui\outcome\node\"
+"C:\ATDI\HTZ warfare x64\HTZwx64.exe" "C:\HTZ PROJECT\INP.PRO" -ADMIN 1025 4 "C:\gui\outcome\NODE"
 
 rem : Analysing the connection matrix and establish layer 3 routing, RF analysis and so on..
-"C:\ATDI\HTZ warfare x64\Mesh\Mesh.exe" -i "C:\gui\outcome\node\*NODES.CSV" -t -137 -h 4
+"C:\ATDI\HTZ warfare x64\Mesh\Mesh.exe" -i "C:\gui\outcome\NODE\*NODES.CSV" -t -137 -h 4
 
---
+++ batch/P2P_hctr.bat
@@ -0,0 +1,10 @@
+move "C:\gui\outcome\P2P\*.*" "C:\gui\outcome\P2P\Backup\" 
+
+rem coverage calculation
+rem "C:\ATDI\HTZ warfare x64\HTZwx64.exe" "C:\HTZ PROJECT\INP.PRO" -ADMIN 1022 1 30
+
+rem : P2P (layer 1) RF analysis. Building the connection Matrix
+"C:\ATDI\HTZ warfare x64\HTZwx64.exe" "C:\HTZ PROJECT\INP.PRO" -ADMIN 1024 1000 "C:\gui\outcome\P2P\hctr\"
+
+rem : Analysing the connection matrix and establish layer 3 routing, RF analysis and so on..
+rem "C:\ATDI\HTZ warfare x64\Mesh\Mesh.exe" -i "C:\gui\outcome\p2p\*P2P.CSV" -t -120 -h 4
